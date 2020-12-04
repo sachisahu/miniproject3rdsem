fc49b05458554bf7be17b9a2c6bc4cbe6dec9797
Sachidanand Sahu <sachisahu0501@gmail.com>
2020-12-04 10:58:00 +0530

    updated

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,35 @@
 function signup(){
-    alert("Sigmup Successful")
+    //Email Validation...............................
+    const email =  document.getElementById('email').value; 
+       let regex =/^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/; 
+       let s = email; 
+       if(regex.test(s)){ 
+        } 
+        else{ 
+            alert("inv")
+        } 
+
+    // Password Validation.............................
+    let newPassword = document.getElementById('Pass').value;
+    let minNumberofChars = 6;
+    let maxNumberofChars = 16;
+    let regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    
+    if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
+        alert("Password Not Acceptable")
+    }
+    if(!regularExpression.test(newPassword)) {
+        alert("password should contain atleast one number and one special character");
+        return false;
+    }
+    //Phone Validation................................(under conc)
+    let phone = document.getElementById('phone').value;
+    let regExp = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
+    let phone1 = phone.match(regExp);
+    if (phone1) {
+      alert('yes');
+     return true;
+    }
+     alert('no');
+     return false;
 }
